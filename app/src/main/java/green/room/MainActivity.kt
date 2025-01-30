d5c3e07b0863abe1a3b8cff0535df0f370dc8b9a
minseok-kang <bbde1861@gmail.com>
2025-01-30 18:24:59 +0900

    [Feature] MainActivity.kt work with firebaseAnalytics

--- app/src/main/java/green/room/MainActivity.kt
+++ app/src/main/java/green/room/MainActivity.kt
@@ -2,29 +2,48 @@ package green.room
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
 import dagger.hilt.android.AndroidEntryPoint
+import green.room.notification.MessagingService
 import green.room.onboarding.OnboardingActivity
 import green.room.preference.DevicePreference
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var devicePreference: DevicePreference
+    @Inject
+    lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        firebaseAnalytics = Firebase.analytics
+
+        val messagingService = MessagingService()
+        messagingService.getToken()
+
+        Log.i("MainActivity", "Fetching FCM token...")
 
         CoroutineScope(Dispatchers.IO).launch {
-            DevicePreference.clearAll(this@MainActivity)
+            if (BuildConfig.DEBUG) {
+                devicePreference.clearAll()
+            }
         }
+        moveToOnboarding()
+    }
 
-        // OnboardingActivity로 이동
+
+    private fun moveToOnboarding() {
         val intent = Intent(this, OnboardingActivity::class.java)
         startActivity(intent)
-
-
-        // MainActivity 종료
         finish()
     }
 }
