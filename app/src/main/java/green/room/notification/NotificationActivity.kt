5719d97b70f85aa4c05769bbf2bb326d5b0a36bc
minseok-kang <bbde1861@gmail.com>
2025-02-10 16:39:15 +0900

    [Routing] Add Notification page back routing

--- app/src/main/java/green/room/notification/NotificationActivity.kt
+++ app/src/main/java/green/room/notification/NotificationActivity.kt
@@ -1,15 +1,24 @@
 package green.room.notification
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import dagger.hilt.android.AndroidEntryPoint
 import green.room.R
+import green.room.profile.ProfileActivity
 
 @AndroidEntryPoint
 class NotificationActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+        val notificationBackButton : Toolbar = findViewById(R.id.notification_appbar)
 
+        notificationBackButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(intent)
+        }
     }
 }
