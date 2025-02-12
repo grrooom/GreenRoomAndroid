d96aa7bf21652bb2132e540f6b001a739572acf8
minseok-kang <bbde1861@gmail.com>
2025-02-12 17:48:52 +0900

    [Feature] Add API Endpoints

--- app/src/main/java/green/room/common/network/ApiEndpoints.kt
+++ app/src/main/java/green/room/common/network/ApiEndpoints.kt
@@ -6,4 +6,7 @@ object ApiEndpoints {
     const val EMAIL_AUTHENTICATION = BuildConfig.BASE_URL + "/api/auth/email/authentication"
     const val EMAIL_TOKEN_AUTHENTICATION = BuildConfig.BASE_URL + "/api/auth/email/token/authentication"
     const val SIGN_UP = BuildConfig.BASE_URL + "/api/auth/signup"
+    const val REISSUE_USER_TOKENS = BuildConfig.BASE_URL + "/api/auth/tokes" // Renew client access, refresh tokens
+    const val HOME_INFORMATION = BuildConfig.BASE_URL + "/api/greenroom/info"
+    const val PROFILE_INFORMATION = BuildConfig.BASE_URL + "/api/users/info"
 }
