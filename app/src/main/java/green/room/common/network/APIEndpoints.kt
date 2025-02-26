d239cf3e095482c1ce9e92ba78d70e6b1698d82b
minseok-kang <bbde1861@gmail.com>
2025-02-26 21:55:55 +0900

    [Add] Api endpoints, "/api/auth/tokens", "/api/users/profile-image"

--- app/src/main/java/green/room/common/network/APIEndpoints.kt
+++ app/src/main/java/green/room/common/network/APIEndpoints.kt
@@ -2,11 +2,12 @@ package green.room.common.network
 
 import green.room.BuildConfig
 
-object ApiEndpoints {
+object APIEndpoints {
     const val EMAIL_AUTHENTICATION = BuildConfig.BASE_URL + "/api/auth/email/authentication"
     const val EMAIL_TOKEN_AUTHENTICATION = BuildConfig.BASE_URL + "/api/auth/email/token/authentication"
     const val SIGN_UP = BuildConfig.BASE_URL + "/api/auth/signup"
-    const val REISSUE_USER_TOKENS = BuildConfig.BASE_URL + "/api/auth/tokes" // Renew client access, refresh tokens
+    const val REISSUE_USER_TOKENS = BuildConfig.BASE_URL + "/api/auth/tokens" // Renew client access, refresh tokens
     const val HOME_INFORMATION = BuildConfig.BASE_URL + "/api/greenroom/info"
     const val PROFILE_INFORMATION = BuildConfig.BASE_URL + "/api/users/info"
+    const val PROFILE_IMAGE_UPDATE = BuildConfig.BASE_URL + "/api/users/profile-image"
 }
